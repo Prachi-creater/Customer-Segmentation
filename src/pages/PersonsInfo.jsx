0433a14059d68e664d1d70eab87539d94d4f2446
letzzBuild <letzzBuild@gmail.com>
2021-06-09 11:20:10 +0530

    customer table added

--- src/pages/PersonsInfo.jsx
+++ src/pages/PersonsInfo.jsx
@@ -1,43 +1,219 @@
-import * as React from 'react';
-import { DataGrid } from '@material-ui/data-grid';
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import HomeIcon from '@material-ui/icons/Home';
+import AddBoxIcon from '@material-ui/icons/AddBox';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import { BrowserRouter as Router, Link, withRouter } from "react-router-dom";
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import '../pages/styles.css'
+import StickyHeadTable from './components/Table';
 
-const columns = [
-  { field: 'id', headerName: 'ID', width: 100 },
-  { field: 'firstName', headerName: 'First name', width: 170 },
-  { field: 'lastName', headerName: 'Last name', width: 170 },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 150,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.getValue('firstName') || ''} ${params.getValue('lastName') || ''}`,
-  },
-];
+const drawerWidth = 240;
+const icons = [
+    <HomeIcon />,
+    <AccountCircleIcon />,
+    <AddBoxIcon />,
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
+]
 
-export default function PersonInfo() {
-  return (
-    <div style={{ height: 400, width: '90%' }}>
-      <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection />
-    </div>
-  );
+const routes = ['/admin/dashboard', '/admin/dashboard/about', '/admin/dashboard/newdeals','/admin/dashboard/personinfo'];
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex'
+    },
+    container: {
+      maxHeight: 440,
+    },
+    link: {
+        textDecoration: 'none',
+        color: theme.palette.text.primary
+    },
+    appBar: {
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    hide: {
+        display: 'none',
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    drawerHeader: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: theme.spacing(0
+            , 1),
+        // necessary for content to be below app bar
+        ...theme.mixins.toolbar,
+        justifyContent: 'flex-end',
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        marginLeft: -drawerWidth,
+    },
+    contentShift: {
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginLeft: 0,
+    },
+}));
+
+function Dashboard({ history }) {
+    const classes = useStyles();
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(false);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
+    return (
+
+
+        <div className={classes.root}>
+            <Router>
+                <CssBaseline />
+
+                <AppBar
+                    position="fixed"
+                    className="appbar"
+                    className={clsx(classes.appBar, {
+                        [classes.appBarShift]: open,
+                    })}
+                    style={{ backgroundColor: '#107AE0' }}
+                >
+
+                    <Toolbar>
+
+                        <IconButton
+                            color="inherit"
+                            aria-label="open drawer"
+                            onClick={handleDrawerOpen}
+                            edge="start"
+                            className={clsx(classes.menuButton, open && classes.hide)}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <div className='item-1'>
+                            <Typography variant="h6" noWrap>
+                                Admin Dashboard
+                        </Typography>
+                        </div>
+
+                        <div className='item-2'>
+
+                            <IconButton>
+                                <Avatar src="/broken-image.jpg" />
+
+                            </IconButton>
+                        </div>
+
+                        <div className='item-3'>
+                            <Button color="inherit">Logout</Button>
+                        </div>
+
+
+                    </Toolbar>
+
+                </AppBar>
+
+
+
+                <Drawer
+                    className={classes.drawer}
+                    variant="persistent"
+                    anchor="left"
+                    open={open}
+                    classes={{
+                        paper: classes.drawerPaper,
+                    }}
+                >
+                    <div className={classes.drawerHeader}>
+                        <IconButton onClick={handleDrawerClose}>
+                            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                        </IconButton>
+                    </div>
+                    <Divider />
+                    <List>
+                        <ListItem button key={0} onClick={() => history.push(routes[0])}>
+                            <ListItemIcon>{icons[0]}</ListItemIcon>
+                            <ListItemText primary={"Home"} />
+                        </ListItem>
+                        <ListItem button key={1} onClick={() => history.push(routes[1])}>
+                            <ListItemIcon>{icons[1]}</ListItemIcon>
+                            <ListItemText primary={"About"} />
+                        </ListItem>
+                        <ListItem button key={2} onClick={() => history.push(routes[2])}>
+                            <ListItemIcon>{icons[2]}</ListItemIcon>
+                            <ListItemText primary={"New Deals"} />
+                        </ListItem>
+                        
+                    </List>
+                    <Divider />
+
+                </Drawer>
+            </Router>
+            <main className={classes.content}>
+                <Toolbar />
+                <Typography paragraph>
+                    <div className="table">
+                      <div><h1>All Customer Details</h1></div>
+                    <StickyHeadTable></StickyHeadTable>
+                    </div>
+                </Typography>
+
+            </main>
+        </div>
+
+
+    );
 }
+
+export default withRouter(Dashboard);
+
+
